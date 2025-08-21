fcb336bca4adf5f944fc78e58dc6c8d6e3f52134
CodeMasters12218 <eowinmur@gmail.com>
2025-08-21 16:14:02 +0200

    Preparación de la app para internacionalización.

---
+++ i18n.js
@@ -0,0 +1,29 @@
+﻿// src/i18n.js
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import * as RNLocalize from "react-native-localize";
+
+// Importar recursos iniciales
+import en from "./locales/en/translation.json";
+import es from "./locales/es/translation.json";
+
+// Detectar idioma del dispositivo
+const locales = RNLocalize.getLocales();
+const systemLanguage = Array.isArray(locales) && locales.length > 0
+  ? locales[0].languageCode
+  : "en";
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: { translation: en },
+      es: { translation: es },
+    },
+    lng: systemLanguage,  // idioma inicial
+    fallbackLng: "en",
+    compatibilityJSON: "v3", // recomendado en RN
+    interpolation: { escapeValue: false },
+  });
+
+export default i18n;
